78c8028e73a87d08ed19b792f867af24da808ca5
bom <qh0309@naver.com>
2024-03-29 17:11:22 +0900

    로그인, 메인 페이지 구현

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,66 @@
-// 보문이가 작업할 곳
-function Login() {
-    return(
-        <></>
+import React from 'react';
+import '../styles/Login.css';
+
+function LoginPage() {
+    return (
+        <div className="loginentireframe">
+            <div className="logo">
+                {/* 로고 이미지 또는 텍스트를 여기에 추가 */}
+            </div>
+            <div className="logintext">
+                로그인 
+            </div>
+            <div className="emailframe">
+                <span className="emailtext">
+                    이메일                
+                </span>
+                {/* 이메일 입력 필드 */}
+            </div>
+            <div className="pwframe">
+                <span className="pwtext">
+                    비밀번호               
+                </span>
+                {/* 비밀번호 입력 필드 */}
+            </div>
+            <div className="loginframe">
+                <button className="login">
+                    로그인
+                </button>
+                {/* 로그인 버튼 */}
+            </div>
+            <div className="widthline">
+                {/* 수평 라인 */}
+            </div>
+            <div className="membershiptext">
+                회원가입
+            </div>
+            <div className="imgframe">
+                <div className="naverimg">
+                    {/* 네이버 이미지 */}
+                </div>
+                <div className="kakaotalkimg">
+                    {/* 카카오톡 이미지 */}
+                </div>
+                <div className="googleimg">
+                    {/* 구글 이미지 */}
+                </div>
+            </div>
+            <div className="signupemail">
+                이메일로 가입하기
+            </div>
+            <div className="emailpwsearchframe">
+                <div className="searchid">
+                    아이디 찾기
+                </div>
+                <div className="lengthline">
+                    {/* 수직 라인 */}
+                </div>
+                <div className="searchpw">
+                    비밀번호 찾기
+                </div>
+            </div>
+        </div>
     );
 }
 
-export default Login;
+export default LoginPage;
---
+++ src/pages/Main.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import '../styles/Main.css';
+
+function MainPage() {
+    const handleLoginClick = () => {
+        window.location.href = "login";
+    };
+
+    const handleThemeWatchClick = () => {
+        window.location.href = "themewatch.html";
+    };
+
+    const handleThemeMakeClick = () => {
+        window.location.href = "thememake.html";
+    };
+
+    const handleCommunityClick = () => {
+        window.location.href = "community.html";
+    };
+
+    const handleThemeSaveClick = () => {
+        window.location.href = "themesave.html";
+    };
+
+    return (
+        <div className="entire">
+            <div className="searchbg">
+                <div className="logo">
+                </div>
+                <div className="searchframe">
+                    <div className="searchtext">
+                        테마명 / 작가명 검색하기
+                    </div>
+                    <div className="search">
+                        <img className="serchimg" />
+                    </div>
+                </div>
+                <div className="login" id="loginLink" onClick={handleLoginClick}>
+                    로그인 / 회원가입
+                </div>
+            </div>
+            <div className="thememenu">
+                <span className="themewatch" id="ThemeWatchBtn" onClick={handleThemeWatchClick}>
+                    테마 둘러보기
+                </span>
+                <span className="thememake" id="ThemeMakeBtn" onClick={handleThemeMakeClick}>
+                    테마 제작
+                </span>
+                <span className="community" id="CommunityBtn" onClick={handleCommunityClick}>
+                    커뮤니티
+                </span>
+                <span className="themesave" id="ThemeSaveBtn" onClick={handleThemeSaveClick}>
+                    내 보관함
+                </span>
+            </div>
+            <div className="uploadbg">
+                <div className="myuploadthemeframe">
+                    <span className="myuploadtheme">
+                        내가 올린 테마 모아보기
+                    </span>
+                </div>
+                <div className="themelikeframe">
+                    <span className="themelike">
+                        좋아요한 테마 모아보기
+                    </span>
+                </div>
+                <div className="themeuploadfrmae">
+                    <span className="themeupload">
+                        테마 올리기
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MainPage;
