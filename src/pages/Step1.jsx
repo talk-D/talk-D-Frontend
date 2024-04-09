670052529081df6e1822323fb299f1a46bd9459e
bom <qh0309@naver.com>
2024-04-09 11:25:38 +0900

    step1구현

--- src/pages/Step1.jsx
+++ src/pages/Step1.jsx
@@ -25,38 +25,23 @@ function Step1Page() {
         </div>
       </div>
       <div className="themeinfo">테마 정보</div>
-        <div className="themename-kor">
-        <div className="themename-kor-2">
-          <div className="rectangle" />
-          <div className="text-wrapper-2">테마 이름(한글)</div>
-        </div>
-        </div>
-        <div className="themename-eng">
-          <div className="rectangle-2" />
-          <div className="text-wrapper-3">테마 이름(영문)</div>
-        </div>
-        <div className="theme-ID">
-          <div className="overlap-2">
-            <div className="text-wrapper-4">No. 100</div>
-          </div>
-          <div className="text-wrapper-5">테마ID</div>
-        </div>
-        <div className="themeversion">
-          <div className="overlap-3">
-            <div className="text-wrapper-6">10.3.5</div>
-          </div>
-          <div className="text-wrapper-7">테마 버전</div>
-        </div>
-      </div>
+      <div className="themename-korframe" />
+      <div className="themename-kor">테마 이름(한글)</div>
+      <div className="themename-engframe" />
+      <div className="themename-eng">테마 이름(영문)</div>
+      <div className="theme-i-dframe" />
+      <div className="theme-ID">테마ID</div>
+      <div className="themeversionframe" />
+      <div className="themeversion">테마 버전</div>
     </div>
     <div className="step-wrapper">
       <div className="step">STEP 1. 테마 정보</div>
     </div>
-    <div className="overlap-3">
+    <div className="overlap-2">
       <div className="step-2">STEP 2. 테마 제작</div>
     </div>
     <div className="thumbnail-wrapper">
-      <div className="text-wrapper-8">썸네일</div>
+      <div className="text-wrapper-2">썸네일</div>
     </div>
     <div className="pw-wrapper">
       <div className="pw">비밀번호</div>
@@ -65,18 +50,19 @@ function Step1Page() {
       <div className="freindlist">친구 목록</div>
     </div>
     <div className="chattingroom-wrapper">
-      <div className="text-wrapper-8">채팅방</div>
+      <div className="text-wrapper-2">채팅방</div>
     </div>
     <div className="tapbar-wrapper">
       <div className="tapbar">탭바</div>
     </div>
-    <div className="overlap-4">
+    <div className="overlap-3">
       <div className="step-2">STEP 3. 테마 다운</div>
     </div>
     <div className="notification-wrapper">
       <div className="notification">알림창</div>
     </div>
   </div>
+</div>
     );
 };
 export default Step1Page;
